be60943cc32036658c56fa39ff84171ea35a482f
Ken Moore <ken@ixsystems.com>
2019-10-29 15:07:12 -0400

    Add a pre-testing version of a PagerDuty plugin.

---
+++ provider-plugins/pagerduty/src/pagerduty.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"os"
+	"io/ioutil"
+	"encoding/json"
+	"github.com/PagerDuty/go-pagerduty"
+	"fmt"
+)
+
+type API struct {
+	Authtoken string	`json:"authtoken"`
+	Type string		`json:"type"`
+	Title string		`json:"title"`
+	From string		`json:"from"`
+}
+
+type AlertText struct {
+	Html string 		`json:"html"`
+	PlainText string	`json:"plain"`
+}
+
+type AlertAPI struct {
+	Text AlertText		`json:"text"`
+	Settings API		`json:"settings"`
+}
+
+func readAPI(path string) AlertAPI {
+  var api AlertAPI
+  tmp, err := ioutil.ReadFile(os.Args[1])
+  if err != nil { fmt.Println("Cannot read API file: ", path) ; os.Exit(1) } //cannot read input JSON
+  err = json.Unmarshal(tmp, &api)
+  if err != nil { fmt.Println("Cannot read API JSON: ", path) ; os.Exit(1) } //cannot read input JSON
+  return api
+}
+
+func mkIncidentFromAPI(api AlertAPI) pagerduty.CreateIncidentOptions {
+  var incident pagerduty.CreateIncidentOptions
+  incident.Type = api.Settings.Type
+  incident.Title = api.Settings.Title
+  incident.Service = nil
+  incident.Priority = nil
+  incident.Body = &pagerduty.APIDetails{ "", api.Text.PlainText }
+  return incident
+}
+
+func main() {
+  // Parse the input API
+  api := readAPI(os.Args[1])
+
+  client := pagerduty.NewClient(api.Settings.Authtoken);
+  ciopts := mkIncidentFromAPI(api) //Create Incident Options
+  _, err := client.CreateIncident( api.Settings.From, &ciopts);
+
+  if err != nil {
+    fmt.Println("Error sending pagerduty incident:", err)
+    os.Exit(1)
+  }
+  os.Exit(0)
+}
